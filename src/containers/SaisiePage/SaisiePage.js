d1089cb050eb2a2705bdbe08ba985f905483671c
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-11-12 10:34:29 +0100

    SaisiePage is now more beautiful!

--- src/containers/SaisiePage/SaisiePage.js
+++ src/containers/SaisiePage/SaisiePage.js
@@ -1,32 +1,37 @@
 import React, { Component } from 'react';
 import { Card, CardHeader, Avatar } from 'material-ui';
-import { Style } from 'radium';
+import Radium, { Style } from 'radium';
 
 import PatientsTable from '../../components/PatientsTable';
 import PatientInput from '../../components/PatientInput';
 
 import styles from './styles';
 
+@Radium
 export default class SaisiePage extends Component {
   render() {
     return (
       <div className="layout vertical">
         <Style rules={styles}/>
-        <Card className="padded">
-          <CardHeader
-            title="Saisie"
-            subtitle="Entrer un nouveau resident"
-            avatar={<Avatar>1</Avatar>}/>
-          <PatientInput {...this.props} />
-        </Card>
+        <div style={[styles.padded]}>
+          <Card className="padded">
+            <CardHeader
+              title="Saisie"
+              subtitle="Entrer un nouveau resident"
+              avatar={<Avatar>1</Avatar>}/>
+            <PatientInput {...this.props} />
+          </Card>
+        </div>
 
-        <Card className="padded">
-          <CardHeader
-            title="Visualisation"
-            subtitle="Liste des couches utilisées pour chaque résident"
-            avatar={<Avatar>2</Avatar>}/>
-          <PatientsTable className="flex" {...this.props} />
-        </Card>
+        <div style={[styles.padded]}>
+          <Card className="padded">
+            <CardHeader
+              title="Visualisation"
+              subtitle="Liste des couches utilisées pour chaque résident"
+              avatar={<Avatar>2</Avatar>}/>
+            <PatientsTable className="flex" {...this.props} />
+          </Card>
+        </div>
       </div>
     );
   }
--- src/containers/SaisiePage/styles.js
+++ src/containers/SaisiePage/styles.js
@@ -1,9 +1,14 @@
 export default {
+  // hack for materiaul ui compontent
   '.padded': {
     padding: '20px',
 
     // overide <Card> css to display <SelectField> <DropDown>
     overflow: 'visible !important',
     zIndex: 'auto !important',
+  },
+
+  padded: {
+    padding: '10px',
   },
 };
